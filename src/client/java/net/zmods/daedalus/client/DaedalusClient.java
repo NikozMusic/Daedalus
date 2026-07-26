11274e2709f4a47d427ae2ae62ecb52c75e163b3
NikozMusic <102382700+NikozMusic@users.noreply.github.com>
2026-07-26 10:01:08 -0400

    Initial commit

---
+++ src/client/java/net/zmods/daedalus/client/DaedalusClient.java
@@ -0,0 +1,10 @@
+package net.zmods.daedalus.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DaedalusClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/zmods/daedalus/client/DaedalusDataGenerator.java
@@ -0,0 +1,11 @@
+package net.zmods.daedalus.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DaedalusDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
